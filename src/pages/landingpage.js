a5f1515c17c9f66b56c3254603110f2922d80014
Rohitg24 <rohitgholap4@gmail.com>
2023-03-07 14:06:17 +0530

    day 4

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -127,7 +127,34 @@ function LandingPage() {
                 </g>
               </svg>
             </a>
-            <a></a>
+            <a>
+              <svg
+                fill="#ffffff"
+                width="30px"
+                height="30px"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="#ffffff"
+              >
+                <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <title />{" "}
+                  <g id="about">
+                    {" "}
+                    <path d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z" />{" "}
+                    <path d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z" />{" "}
+                  </g>{" "}
+                </g>
+              </svg>
+            </a>
           </div>
         </header>
       </div>
